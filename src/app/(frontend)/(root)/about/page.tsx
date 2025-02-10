7d523e15286b797aafe57a6ef3b256c832cfde4b
s-kvng <kvngnathan8420@gmail.com>
2025-02-10 15:42:45 +0000

    add hero section

---
+++ src/app/(frontend)/(root)/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import HeroSection from '@/components/hocs/AboutComponents/HeroSection'
+
+const AboutPage = () => {
+  return (
+    <HeroSection />
+  )
+}
+
+export default AboutPage
