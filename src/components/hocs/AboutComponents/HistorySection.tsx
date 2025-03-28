d9309e57a31e1cd043064e59c682b0f727bdb1a7
s-kvng <kvngnathan8420@gmail.com>
2025-03-28 10:35:34 +0000

    update about page 🚠

--- src/components/hocs/AboutComponents/HistorySection.tsx
+++ src/components/hocs/AboutComponents/HistorySection.tsx
@@ -1,12 +1,12 @@
-"use client"
+'use client'
 
-import { motion } from "motion/react"
+import { motion } from 'motion/react'
 
 export default function HistorySection() {
   return (
-    <div className="py-24" id="history">
+    <div className="bg-[#0f9e99] py-24" id="history">
       <div className="container mx-auto px-4">
-        <div className="grid md:grid-cols-2 gap-12">
+        <div className="grid gap-12 md:grid-cols-2">
           <motion.div
             initial={{ opacity: 0, x: 100 }}
             whileInView={{ opacity: 1, x: 0 }}
@@ -14,11 +14,14 @@ export default function HistorySection() {
             viewport={{ once: true }}
             className="space-y-4"
           >
-            <h2 className="text-3xl font-bold">History of Star Hacks</h2>
-            <p className="text-gray-600 leading-relaxed">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-              ea commodo consequat.
+            <h2 className="text-3xl font-bold text-[#efe9e0]">History of Star Hacks</h2>
+            <p className="leading-relaxed text-[#efe9e0]">
+              School Name was established in Year with the vision of creating an educational
+              institution that fosters excellence, innovation, and character development. Since its
+              inception, the school has grown into a beacon of knowledge, nurturing young minds and
+              shaping future leaders. With a commitment to academic rigor and holistic education, we
+              continue to uphold our legacy of excellence while embracing modern educational
+              advancements.
             </p>
           </motion.div>
         </div>
@@ -26,4 +29,3 @@ export default function HistorySection() {
     </div>
   )
 }
-
